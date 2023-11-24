64a20a60a0a6a225014d451e4f924252a0c06a4d
Attull <atul852798@gmail.com>
2023-11-24 16:08:39 +0530

    add API interagtion in APP.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState({})
+  const fetchData = () =>{
+    fetch('https://api.chucknorris.io/jokes/random')
+    .then(res => res.json())
+    .then(data =>{
+      setData(data)
+    })
+  }
+
+  useEffect(()=>{
+    fetchData()
+  },[])
+
   return (
     <div className="App">
-   
+        {data.value}
     </div>
   );
 }
